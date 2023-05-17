52bba250e09e8be5a6e036b3b528dadaabcb899f
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-05-17 16:09:47 +0100

    Update README file

--- spec/morse_message_spec.rb
+++ spec/morse_message_spec.rb
@@ -1,36 +1,38 @@
-Morse_Symbols = { "A" => ".-", "B" => "-...", "C" => "-.-.", "D" => "-..", "E" => ".", "F" => "..-.", "G" => "--.", "H" => "....", "I" => "..", "J" => ".---", "K" => "-.-", "L" => ".-..", "M" => "--", "N" => "-.", "O" => "---", "P" => ".--.", "Q" => "--.-", "R" => ".-.", "S" => "...", "T" => "-", "U" => "..-", "V" => "...-", "W" => ".--", "X" => "-..-", "Y" => "-.--", "Z" => "--..",}
+MORSE_SYMBOLS = { 'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.', 'G' => '--.',
+                  'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..', 'M' => '--', 'N' =>
+                   '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-',
+                  'R' => '.-.', 'S' => '...', 'T' => '-', 'U' => '..-',
+                  'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..' }.freeze
 
 def decode_char(symbol)
-   Morse_Symbols.each{ |key, value| 
-    if value == symbol
-        return key
-    end
-    }
+  MORSE_SYMBOLS.each do |key, value|
+    return key if value == symbol
+  end
 end
 
 # puts decode_char("-...")
 # puts decode_char("..-.")
 
 def decode_word(symbol_word)
-    symbol_array = symbol_word.split(' ')
-    word_array = []
-    symbol_array.each{ |symbol|
-        word_array.push(decode_char(symbol))
-    }
-    return word = word_array.join('')
+  symbol_array = symbol_word.split
+  word_array = []
+  symbol_array.each do |symbol|
+    word_array.push(decode_char(symbol))
+  end
+  word_array.join
 end
 
 # puts decode_word("-- -.--")
 
 def decode(sentence)
-    sentence_symbols_array = sentence.split('   ')
-    sentence_words_array = []
-    sentence_symbols_array.each{ |word| 
-        sentence_words_array.push(decode_word(word))
-    }
-    return sentence = sentence_words_array.join(' ')
+  sentence_symbols_array = sentence.split('   ')
+  sentence_words_array = []
+  sentence_symbols_array.each do |word|
+    sentence_words_array.push(decode_word(word))
+  end
+  sentence_words_array.join(' ')
 end
 
 
 # print decode("-- -.--   -. .- -- .")
-print decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
